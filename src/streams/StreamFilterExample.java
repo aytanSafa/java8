9b956d1d142639411d6438e39efc6838291a2442
safaa <aytansafa@gmail.com>
2023-03-26 00:40:22 +0300

    StreamFilterExample added

---
+++ src/streams/StreamFilterExample.java
@@ -0,0 +1,22 @@
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamFilterExample {
+
+    public static List<Student> filterStudents(){
+
+       return StudentDataBase.getAllStudents().stream()
+                .filter(student -> student.getGender().equals("female"))
+                .collect(Collectors.toList());
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(filterStudents());
+    }
+}
