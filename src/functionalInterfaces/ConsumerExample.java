a00c02f7fe4d3a096f4fcd3a2c82587bc3abe5da
safaa <aytansafa@gmail.com>
2023-03-25 16:21:40 +0300

    pritname method added and Consumer instance was implemented

--- src/functionalInterfaces/ConsumerExample.java
+++ src/functionalInterfaces/ConsumerExample.java
@@ -1,12 +1,23 @@
 package functionalInterfaces;
 
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
 
+    static void  printName(){
+        Consumer<Student> c2 = (s) -> System.out.println(s.toString());
+        List<Student> studentList =  StudentDataBase.getAllStudents();
+        studentList.forEach(c2);
+    }
     public static void main(String[] args) {
         Consumer<String> c1 = (s -> System.out.println(s.toUpperCase()));
         c1.accept("java8");
+
+        printName();
     }
 
 
