745eefc77648c42ff7f0bebcec26d9ddbb9866eb
safaa <aytansafa@gmail.com>
2023-03-25 17:37:13 +0300

    PredicateStudentExample Added

---
+++ src/functionalInterfaces/PredicateStudentExample.java
@@ -0,0 +1,50 @@
+package functionalInterfaces;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateStudentExample {
+
+
+   static Predicate<Student> p1 = (student -> student.getGradeLevel() >= 3);
+   static Predicate<Student> p2 = (student -> student.getGpa() >= 3.9);
+    public static  void filterStudentByGradeLevel(){
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach(student -> {
+            if (p1.test(student))
+                System.out.println(student);
+        });
+
+    }
+
+
+    public static  void filterStudentByGpa(){
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach(student -> {
+            if (p2.test(student))
+                System.out.println(student);
+        });
+
+    }
+
+    public static void  filterStudents(){
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach(student -> {
+            if (p1.and(p2).test(student))
+                System.out.println(student);
+        });
+    }
+    public static void main(String[] args) {
+        filterStudentByGradeLevel();
+        System.out.println("-----------------");
+        filterStudentByGpa();
+        System.out.println("-----------------");
+        filterStudents();
+    }
+}
