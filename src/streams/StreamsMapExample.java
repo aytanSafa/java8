18f3e622d5594a5267e31f16fe7ac653d574408c
safaa <aytansafa@gmail.com>
2023-03-26 00:29:53 +0300

    StreamsMapExample added

---
+++ src/streams/StreamsMapExample.java
@@ -0,0 +1,23 @@
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsMapExample {
+
+    public static List<String> nameList(){
+        return StudentDataBase.getAllStudents().stream()
+                .map(Student::getName)
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nameList());
+    }
+
+
+}
